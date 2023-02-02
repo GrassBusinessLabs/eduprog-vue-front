52e46f963c157dc03f51b86dac8df8574aa0becc
nypzlst <nyshtoprivet@gmail.com>
2023-02-02 15:13:43 +0200

    add base for tabel
    create base for tabel and add items list and select this items(select need fix)

--- src/views/pages/edit-document/EditDocumentSequence.vue
+++ src/views/pages/edit-document/EditDocumentSequence.vue
@@ -1,16 +1,56 @@
-<script setup>
-import TableBasic from '@/views/user-interface/tables/TableBasic.vue'
+<script>
+export default {
+  name: 'Uhuy',
+ 
+  data() {
+    return {
+      items: [
+        { COP: 'Філософія', credit_COP: "6", FC_COP: 'Залік' },
+        { COP: 'Математика', credit_COP: "6", FC_COP: 'Залік' }
+      ],
+      prost:['Філософія','Математика','qwewqewqeqw','ewqeqwewq']   //З сервера має надходити массив з доступними предметами
+   
+    }
+  }
+}
 
 </script>
 
 <template>
-  <VRow>
-    <!-- basic -->
-    <VCol cols="12">
-      <VCard title="Basic">
-        <TableBasic />
-      </VCard>
-    </VCol>
-  </VRow>
+  <VTable>
+    <thead class="thead-light">
+    <tr>
+      <th class="text-center">
+        Назва документу
+      </th>
+    </tr>
+  </thead>
+  </VTable>
+  <VTable>
+    <thead class="thead-light">
+      <tr>
+        <th>Код н/д</th>
+        <th>Назва КОП</th>
+        <th>Залежності</th>     
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="(item, index) in items"
+        :key="index"
+      >
+      <td>{{ 'ОК ' + index }}</td>
+        <td >
+          <span>{{ item.COP }}</span>
+          <span></span>
+          
+         </td> 
+         <td>
+          <VSelect label="select" :items="prost" persistent-hint multiple  ></VSelect> <!--fix position Vselect, his ride on page -->
+         </td>    
+    </tr>
+    </tbody>
+  </VTable>
+
 </template>
 
