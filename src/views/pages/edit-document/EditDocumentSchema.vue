82ebe863ad5d871f7c34c384472f425f6fd139c1
KAZAHv2 <krezy.mashroom@gmail.com>
2023-02-05 17:49:23 +0200

    Fix Schema

--- src/views/pages/edit-document/EditDocumentSchema.vue
+++ src/views/pages/edit-document/EditDocumentSchema.vue
@@ -1,3 +1,4 @@
+
 <template>
   <VTable>
     <thead>
@@ -6,7 +7,12 @@
           rowspan="2"
           class="text-center"
         >
-          Дисципліни
+          <p>Дисципліни</p>
+          <VBtn
+            icon="mdi-plus"
+            size="x-small"
+            @click="add"
+          />
         </th>
         <th
           colspan="2"
@@ -61,6 +67,288 @@
         </th>
       </tr>
     </thead>
+    <tbody>
+      <tr
+        v-for="(item, index) in itemsS"
+        :key="index">
+        <td>
+          <div style='text-align: center'>
+            <span v-if="editIndex !== index" >{{ item.DS }}</span>
+            <span v-if="editIndex === index">
+            <input  class="border-solid"   v-model="item.DS" >
+          </span>
+          </div>
+          <div style='text-align: center; margin-top: 5%; margin-bottom: 5%' >
+          <span v-if="editIndex !== index">
+            <VMenu
+              bottom
+              left
+              activator="parent"
+            >
+              <template #activator="{ on, attrs }">
+                <VBtn
+                  dark
+                  icon
+                  v-bind="attrs"
+                  :shaped="false"
+                  size="x-small"
+                  v-on="on"
+                >
+                  <VIcon>mdi-dots-horizontal</VIcon>
+                </VBtn>
+              </template>
+
+              <VList>
+                <VListItem link @click="edit(item, index)" >
+                  <template #prepend>
+                    <VIcon
+                      class="me-2"
+                      icon="mdi-pencil"
+                      size="22"
+                    />
+                  </template>
+
+
+                  <VListItemTitle>
+                    Редагувати
+                  </VListItemTitle>
+                </VListItem>
+                <VListItem link @click="remove(item, index)" >
+                  <template #prepend>
+                    <VIcon
+                      class="me-2"
+                      icon="mdi-trash-can"
+                      size="22"
+                    />
+                  </template>
+
+                  <VListItemTitle>
+                    Видалити
+                  </VListItemTitle>
+                </VListItem>
+              </VList>
+            </VMenu>
+          </span>
+          <span v-else>
+            <VMenu
+              bottom
+              left
+              activator="parent"
+            >
+              <template #activator="{ on, attrs }">
+                <VBtn
+                  dark
+                  icon
+                  v-bind="attrs"
+                  :shaped="false"
+                  size="x-small"
+                  v-on="on"
+                >
+                  <VIcon>mdi-dots-horizontal</VIcon>
+                </VBtn>
+              </template>
+
+              <VList>
+                <VListItem link @click="save(item)" >
+                  <template #prepend>
+                    <VIcon
+                      class="me-2"
+                      icon="mdi-pencil"
+                      size="22"
+                    />
+                  </template>
+
+
+                  <VListItemTitle >
+                    Зберегти
+                  </VListItemTitle>
+                </VListItem>
+                <VListItem @click="cancel(item)" link>
+                  <template #prepend>
+                    <VIcon
+                      class="me-2"
+                      icon="mdi-trash-can"
+                      size="22"
+                    />
+                  </template>
+
+                  <VListItemTitle >
+                    Відмінити
+                  </VListItemTitle>
+                </VListItem>
+              </VList>
+            </VMenu>
+          </span>
+          </div>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  :items="items"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+        <td>
+          <VContainer fluid>
+            <VRow>
+              <VCol cols="12">
+                <VCombobox
+                  v-model="select"
+                  hide-selected
+                  multiple
+                  small-chips
+                />
+              </VCol>
+            </VRow>
+          </VContainer>
+        </td>
+      </tr>
+    </tbody>
   </VTable>
 </template>
 
+<script>
+export default {
+
+  data() {
+    return {
+      editIndex: null,
+      originalData: null,
+      itemsS: [
+        { DS: "Дисципліни фахової спеціалізації", S1: '', S2: '', S3: '', S4: '', S5: '', S6: '', S7: '', S8: ''},
+      ],
+      select: [
+
+      ],
+      items: [
+        'Programming',
+        'Design',
+        'Vue',
+        'Vuetify',
+      ],
+    }
+  },
+
+  methods: {
+    add() {
+      this.originalData = null
+      this.itemsS.push({ DS: ' ', S1: '', S2: '', S3: '', S4: '', S5: '', S6: '', S7: '', S8: ''})
+      this.editIndex = this.itemsS.length - 1
+    },
+    edit(item, index) {
+      this.originalData = Object.assign({}, item)
+      this.editIndex = index
+    },
+    cancel(item) {
+      this.editIndex = null
+      if (!this.originalData) {
+        this.itemsS.splice(this.itemsS.indexOf(item), 1)
+
+        return
+      }
+      Object.assign(item, this.originalData)
+      this.originalData = null
+    },
+    remove(item, index) {
+      this.itemsS.splice(index, 1)
+    },
+    save(item) {
+      this.originalData = null
+      this.editIndex = null
+    },
+  },
+
+}
+</script>
