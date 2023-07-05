43747a6ccd44c5e1f42e0c642c830dc97c8a7ef4
Maxim <hhbbj63@gmail.com>
2023-07-05 17:02:40 +0300

    fix

--- src/pages/eduprogs/[pages]/sequence.vue
+++ src/pages/eduprogs/[pages]/sequence.vue
@@ -4,7 +4,8 @@ import { onMounted, reactive } from 'vue'
 import { useRoute } from 'vue-router'
 const route = useRoute()
 const eduProgsStore = useEduProgsStore()
-const {components} =storeToRefs(eduProgsStore)
+const { components } = storeToRefs(eduProgsStore)
+console.log(components.value)
 const loading = ref(false)
 const sortedComponents = component => {
   return components.value.mandatory.filter(el => {
@@ -12,74 +13,73 @@ const sortedComponents = component => {
   })
 }
 const selectedComponents = reactive({})
-const selectedComponentsInDB =ref({})
+const selectedComponentsInDB = ref({})
 const possibleComponents = reactive({})
 onMounted(async () => {
   await eduProgsStore.findEduProgById(route.params.pages)
   loading.value = true
   await components.value.mandatory.forEach(el => {
-    selectedComponents[el.id]=[]
+    selectedComponents[el.id] = []
     eduProgsStore.fetchPossibleRelations(el.eduprog_id, el.id).then(res => {
       possibleComponents[el.id] = res
     })
   })
   await eduProgsStore.fetchRelations(route.params.pages).then(res => {
     console.log(selectedComponents)
-    Object.keys(selectedComponents).forEach((componentId) =>{
-      selectedComponents[componentId]=res.filter(el => el.base_comp_id == componentId)
-      selectedComponents[componentId].forEach(el =>{
-      el.id = el.child_comp_id
-    })
+    Object.keys(selectedComponents).forEach(componentId => {
+      selectedComponents[componentId] = res.filter(el => el.base_comp_id == componentId)
+      selectedComponents[componentId].forEach(el => {
+        el.id = el.child_comp_id
+      })
     })
   })
-  selectedComponentsInDB.value = {...selectedComponents}
-  console.log("Селектед", selectedComponents)
+  selectedComponentsInDB.value = { ...selectedComponents }
+  console.log('Селектед', selectedComponents)
   loading.value = false
 })
 
-const activeCombobox= reactive({})
-const handleChanges = (e, component) =>{
-  console.log("Ивент", e)
-  console.log("Компонент", component)
+const activeCombobox = reactive({})
+const handleChanges = (e, component) => {
+  console.log('Ивент', e)
+  console.log('Компонент', component)
   activeCombobox.base_comp_id = component.id
   activeCombobox.child_comp_id = e
 }
-const closeCombobox = async () =>{
-  if(activeCombobox.base_comp_id){
-  const inDb = selectedComponentsInDB.value[activeCombobox.base_comp_id]
-  const onSite = selectedComponents[activeCombobox.base_comp_id]
-  const forDelete = []
-  const forAdd = []
-  console.log("На сайте", selectedComponents[activeCombobox.base_comp_id])
-  console.log("В БД", selectedComponentsInDB.value[activeCombobox.base_comp_id])
-  inDb.forEach(element => {
-  console.log(element.id)
-  if (!onSite.find(e => e.id=== element.id)){
-    forDelete.push(element)
-  }
-  })
-  onSite.forEach(element => {
-  console.log(element.id)
-  if (!inDb.find(e => e.id=== element.id)){
-    forAdd.push(element)
-  }
-  })
-  forAdd.forEach((el)=>{
-    const newRelation = {
-      eduprog_id:+route.params.pages,
-      base_comp_id: activeCombobox.base_comp_id,
-      child_comp_id: el.id
-    }
-    eduProgsStore.createRelation(newRelation)
-    
-  })
-  forDelete.forEach((el)=>{
-    eduProgsStore.deleteRelation(el.base_comp_id, el.child_comp_id)
-  })
-  console.log("На удаление", forDelete)
-  console.log("На добавление", forAdd)
-  activeCombobox.base_comp_id = null
-  activeCombobox.child_comp_id = null
+const closeCombobox = async () => {
+  if (activeCombobox.base_comp_id) {
+    const inDb = selectedComponentsInDB.value[activeCombobox.base_comp_id]
+    const onSite = selectedComponents[activeCombobox.base_comp_id]
+    const forDelete = []
+    const forAdd = []
+    console.log('На сайте', selectedComponents[activeCombobox.base_comp_id])
+    console.log('В БД', selectedComponentsInDB.value[activeCombobox.base_comp_id])
+    inDb.forEach(element => {
+      console.log(element.id)
+      if (!onSite.find(e => e.id === element.id)) {
+        forDelete.push(element)
+      }
+    })
+    onSite.forEach(element => {
+      console.log(element.id)
+      if (!inDb.find(e => e.id === element.id)) {
+        forAdd.push(element)
+      }
+    })
+    forAdd.forEach(el => {
+      const newRelation = {
+        eduprog_id: +route.params.pages,
+        base_comp_id: activeCombobox.base_comp_id,
+        child_comp_id: el.id,
+      }
+      eduProgsStore.createRelation(newRelation)
+    })
+    forDelete.forEach(el => {
+      eduProgsStore.deleteRelation(el.base_comp_id, el.child_comp_id)
+    })
+    console.log('На удаление', forDelete)
+    console.log('На добавление', forAdd)
+    activeCombobox.base_comp_id = null
+    activeCombobox.child_comp_id = null
   }
 }
 </script>
@@ -92,7 +92,7 @@ const closeCombobox = async () =>{
       </tr>
     </thead>
   </VTable>
-  <VTable v-if='components.mandatory > 0'>
+  <VTable>
     <thead class="thead-light">
       <tr>
         <th>Код н/д</th>
@@ -127,15 +127,22 @@ const closeCombobox = async () =>{
       </tr>
     </tbody>
   </VTable>
-  <v-alert
+  <!-- <v-alert
     v-else
     type="info"
     variant="tonal"
     class="my-4"
   >
     Для того щоб тут з'явилися можливі зв'язки, потрібно спочатку заповнити
-    <u><a href="schema" style='color: inherit'> сторінку розподілу по семестрам.</a></u>
-  </v-alert>
+    <u
+      ><a
+        href="schema"
+        style="color: inherit"
+      >
+        сторінку розподілу по семестрам.</a
+      ></u
+    >
+  </v-alert> -->
 </template>
 <route lang="yaml">
 name: sequence
